ecbcbdf480283b513e534e7368aa1eca78992758
Eof19 <Eof19@fsu.edu>
2022-06-02 13:46:35 -0400

    new skill sets 5 and 6

---
+++  SkillSets/Q5_Random_Number_Generator/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        //call static void methods (i.e, no objects, non-value returning)
+        Methods.getRequirements();
+        Methods.getUserPhoneType();
+    }
+}
